aefddc7b44df0945d61ea2631e8f62ff9921d59f
Danilova-Viktoria <danilovaviktorija5@gmail.com>
2022-02-25 17:59:59 +0300

    Вторая версия проекта

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,8 @@
 const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
+function generateDecimalBetween(left, right) {
+    return (Math.random() * (left - right) + right).toFixed(2);
+  }
+
 class Bubble {
     constructor(canvas) {
         this.canvas = canvas;
