54fa2eb4b3c1ec3e97ea0be581739735931cf2e5
Danilova-Viktoria <danilovaviktorija5@gmail.com>
2022-03-09 12:40:26 +0300

    Вторая версия проекта

--- js/animations.js
+++ js/animations.js
@@ -22,7 +22,7 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
   if (event.target.value === 'other') {
@@ -43,3 +43,21 @@ document.querySelector('#budget').addEventListener('change', function handleSele
   document.querySelector('#form form').removeChild(otherInput);
   }
 });
+
+function updateScroll() {
+
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+    }
+
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+ 
+window.addEventListener('scroll', updateScroll);
